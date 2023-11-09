37169f2c3399996c661b8f8a53fe2fde0dba368a
Shamimayesmin <shamimayesmin144@gmail.com>
2023-11-09 21:35:39 +0600

    logout

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,7 +14,7 @@ const LoginPage = () => {
 				<h3>LOGIN</h3>
 				<div className={styles.social_icons}>
 					<GoogleOutlined />
-					<GithubOutlined onClick={() => signIn("github")} />
+					<GithubOutlined onClick={() => signIn("github", {callbackUrl: "http://localhost:3000/"})} />
 				</div>
 				<hr />
 				<form>
